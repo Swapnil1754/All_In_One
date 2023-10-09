05ba55a2cac629fc2352147e14f7b760d8ead1b2
Swapnil-sneo <swapnilsutar2010@gmail.com>
2023-10-09 22:34:51 +0530

    front end register and login pages
    react codding

--- React_FrontEnd/makemyyatraa/src/Pages/Registration/Register.jsx
+++ React_FrontEnd/makemyyatraa/src/Pages/Registration/Register.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import '../CSS/Registration.css';
+import { Container, Typography, TextField, Button, Grid } from '@mui/material'
 const Register = () => {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle registration logic here, e.g., send data to an API
+        console.log("Registration data:", formData);
+    }
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    }
     return (
-        <div>
-
-        </div>
+        <Container maxWidth="sm">
+      <Typography variant="h4" align="center" gutterBottom>
+        Registration
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Username"
+              variant="outlined"
+              fullWidth
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Password"
+              type="password"
+              variant="outlined"
+              fullWidth
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
     )
 }
 export default Register;
